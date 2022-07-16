d5a64814c3b634e3284b58b9ebec74b16d849feb
Victor Lascares <vmlascares@gmail.com>
2022-07-16 14:42:39 -0500

    Consultando guitarras desde la pagina de inicio

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,34 @@
+import styles from '../styles/Tienda.module.css'
 import Layout from '../components/Layout'
+import Guitar from '../components/Guitar';
 
-export default function Home() {
+export default function Home({guitars}) {
     return (
         <Layout
             pagina={'Inicio'}
         >
+            <main className='contenedor'>
+                <h1 className='heading'>Nuestra colección</h1>
+                <div className={styles.listado}>
+                    {guitars.map(guitar => (
+                        <Guitar 
+                            key={guitar.id}
+                            guitar={guitar}
+                        />
+                    ))}
+                </div>
+            </main>
         </Layout>
     )
+}
+
+export async function getServerSideProps() {
+    const url = `${process.env.API_URL}/guitarras?populate=imagen`;
+    const response = await fetch(url);
+    const guitars = await response.json();
+    return {
+        props: {
+            guitars: guitars.data
+        }
+    }
 }
